32561043142c1d5be391f50336edd7df803fa6e4
gracebir <grace.devolop@gmail.com>
2023-03-23 15:00:09 +0200

    sign up logic

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from "@nestjs/common";
+import { PrismaService } from "src/prisma/prisma.service";
+import { AuthDto } from "./dto";
+import * as argon from 'argon2'
 
 @Injectable()
 export class AuthService {
-    login(){
-        return {msg: 'sign up'}
+    constructor(private prisma: PrismaService){}
+    async login(dto: AuthDto){
+       // hash password
+       const hash = await argon.hash(dto.password);
+       // save new user
+       const user = await this.prisma.user.create({
+        data: {
+            email: dto.email,
+            hash
+        }
+       })
+       delete user.hash
+       // return the saves user
+        return user
     }
      signin(){
         return {msg: 'sign in'}
